8a89ffe477bbf714b6ee1d77aaccdf156800fddb
isha <isha.m.l.i.r.25@gmail.com>
2026-04-27 15:07:08 +0530

    feat: add country detail page and useCountry custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import CountryPage from "./pages/CountryPage";
+import "./styles/App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <Router>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/country/:name" element={<div>Country Page</div>} />
-          <Route path="/favourites" element={<div>Favourites Page</div>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+        {/* Country Detail Page */}
+        <Route path="/country/:code" element={<CountryPage />} />
+      </Routes>
+    </Router>
   );
 }
 
